ae2bf5f9a1dc4c3d3977fd8155fbc23664c68ddb
Garri-99 <103282736+Garri-99@users.noreply.github.com>
2023-08-23 10:27:51 +0400

    deploy-repo

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -4,6 +4,7 @@ const {
   DEPLOY_USER,
   DEPLOY_HOST,
   DEPLOY_PATH,
+  DEPLOY_REPO,
   DEPLOY_REF = 'origin/master',
 } = process.env;
 
@@ -17,7 +18,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/Garri-99/web-plus-pm2-deploy.git',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `npm run build && scp -Cr ./build/* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/frontend`,
       'post-deploy': 'cd frontend && pm2 startOrRestart ecosystem.config.js',
